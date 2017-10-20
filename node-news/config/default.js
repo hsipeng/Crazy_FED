cf15822ac7125cbda3d6e3f1a4423ff1f0034a9a
lirawx <lira_wx@outlook.com>
2017-10-20 09:35:11 +0800

    remove personal info

--- node-news/config/default.js
+++ node-news/config/default.js
@@ -1,30 +1,30 @@
 module.exports = {
     port:3000,
     database:{
-        DATABASE: 'thzy',
-        USER: 'thzy',
-        PASSWORD: '!QAZ2wsx1qaz',
+        DATABASE: 'xxxxxxx',
+        USER: 'xxxxx',
+        PASSWORD: 'xxx',
         PORT: '3306',
-        HOST: '47.94.197.113'
+        HOST: 'xxxxx'
     },
     databasedev:{
-        DATABASE: 'thzy12',
+        DATABASE: 'xxxx',
         USER: 'root',
         PASSWORD: '123456',
         PORT: '3306',
         HOST: '127.0.0.1'
     },
     emaildev:{
-        USER: '3067208727@qq.com', //你的邮箱账号
-        PASSWORD: 'oytmxjkdkjkxddej', //你的邮箱密码
+        USER: 'xxx@qq.com', //你的邮箱账号
+        PASSWORD: 'xxxxxxxxxx', //你的邮箱密码
         HOST: 'imap.qq.com', //邮箱服务器的主机地址
         PORT: 993, //邮箱服务器的端口地址
         TLS: true, //使用安全传输协议
         TLSOPTIONS: { rejectUnauthorized: false } //禁用对证书有效性的检查
     },
     email:{
-        USER: 'thzydev@buttonz.cn', //你的邮箱账号
-        PASSWORD: 'Thzy123456', //你的邮箱密码
+        USER: 'xxxx@xxxx.cn', //你的邮箱账号
+        PASSWORD: 'xxxxxx', //你的邮箱密码
         HOST: 'imap.exmail.qq.com', //邮箱服务器的主机地址
         PORT: 993, //邮箱服务器的端口地址
         TLS: true, //使用安全传输协议
