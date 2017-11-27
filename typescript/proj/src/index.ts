25a3c2f24e8a3b4ac076cc601934763b98d0e843
lirawx <lira_wx@outlook.com>
2017-11-27 16:44:40 +0800

    webpack css less sass font ..

--- typescript/proj/src/index.ts
+++ typescript/proj/src/index.ts
@@ -1,6 +1,11 @@
+import  './style.css';
+import  './sass.scss';
+import './less.less';
+import  './css/font-awesome.css';
+
 function area (shape: string, width : number, height :number) {
   var area = width * height;
   return "I'm a " + shape + "width an area of " + area +"cm squared."
 }
 
-document.body.innerHTML = area("rectangle", 30, 15);
+document.getElementById('area').innerHTML = area("rectangle", 30, 15);
